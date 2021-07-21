c975efaf619ad80831cb55133d8b6ef60780041d
cartory <xXcartoryXx@gmail.com>
2021-07-21 07:24:28 -0400

    controllers added

---
+++ src/Controllers/ServiceController.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+import Models.Service;
+import Utils.Controller;
+
+public class ServiceController extends Controller {
+
+    private ServiceController() {
+        super(new Service());
+    }
+}
